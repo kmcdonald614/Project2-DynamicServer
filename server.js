844102fdded4be6dd461c62b7a7736080f8eb6d8
Michael Francomb <mfrancomb@gmail.com>
2022-11-11 08:40:50 -0600

    begin year route
    Begin work on the dynamic rout for /year/:selected_year including a sample SQL query to return yearly data.

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ let path = require('path');
 // NPM modulesllll
 let express = require('express');
 let sqlite3 = require('sqlite3');
+const { response } = require('express');
 
 
 let public_dir = path.join(__dirname, 'public');
 let template_dir = path.join(__dirname, 'templates');
-let db_filename = path.join(__dirname, 'db', 'YOUR_DATABASE_FILE.sqlite3'); // <-- change this
+let db_filename = path.join(__dirname, 'db', 'ghg_emissions.sqlite3'); // <-- change this
 
 let app = express();
 let port = 8000;
@@ -30,7 +31,7 @@ app.use(express.static(public_dir));
 
 // GET request handler for home page '/' (redirect to desired route)
 app.get('/', (req, res) => {
-    let home = ''; // <-- change this
+    let home = '/year/'; // <-- change this
     res.redirect(home);
 });
 
@@ -47,6 +48,33 @@ app.get('/year/:selected_year', (req, res) => {
 });
 */
 
+// GET request handler for year route (e.x., 'localhost:8000/year/2019')
+app.get('/year/:selected_year', (req, res) => {
+    let year = parseInt(req.params.selected_year);
+    console.log(year);
+    fs.readFile(path.join(template_dir, 'year_template.html'), (err, template) => {
+        
+        // placeholders (?) only work for db values, not attributes.
+        // I have tried to protect the query from SQL injection by converting year to int and back to a string.
+        let query = 'SELECT"'+year.toString()+'" AS emissions, country, sector, Gas FROM emissions WHERE country="World" AND Gas="All GHG";'; // <-- change this
+        
+        //query the database
+        db.all(query, [], (err, rows) => {
+            console.log("ERROR: ", err);
+            //grab relevant info
+            console.log("rows", rows);
+
+            // modify template
+            let response = template.toString();
+            response = response.replace("%%EMISSIONS%%", rows[0].emissions);
+
+            //send response
+            res.status(200).type('html').send(response);
+        });
+    })
+})
+
+// Start server
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
 });
